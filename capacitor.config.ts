5846264e16d3ad5233b665d1d15d33feff19101b
ctovar <cartoorellana@gmail.com>
2022-05-02 22:15:59 -0600

    Parcial2

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'TAREAPI',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
